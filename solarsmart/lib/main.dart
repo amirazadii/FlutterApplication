c245952700f42f68bed83c91e936a22badb6281f
amir azadi <aha@risskovteknik.dk>
2025-01-23 12:39:59 -0800

    add container widget

--- solarsmart/lib/main.dart
+++ solarsmart/lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: const Color.fromARGB(255, 245, 245, 245),
-      appBar: AppBar(
-        title: Text('SolarSmart'),
-        backgroundColor: const Color.fromARGB(255, 200, 221, 46),
-      ),
-      body: Center(
-        child: Text('We Are Solarsmart Risskovtek'),
-      ),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 248, 248, 248),
+          appBar: AppBar(
+            title: Text('SolarSmart'),
+            backgroundColor: const Color.fromARGB(255, 200, 221, 46),
+          ),
+          body: SafeArea(
+            child: Container(
+              height: 100,
+              width: 500,
+              margin: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
+              padding: EdgeInsets.all(20),
+              color: Colors.amber,
+              child: Text('We Are Risskovtekik'),
+            ),
+          )),
+    );
+  }
 }
