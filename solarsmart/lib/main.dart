e727c05261f7664b3c456d65a169cc93d8897cc5
amir azadi <aha@risskovteknik.dk>
2025-01-24 04:03:52 -0800

    add title and theme with appbar details

--- solarsmart/lib/main.dart
+++ solarsmart/lib/main.dart
@@ -8,17 +8,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Solar_Smart Risskovteknik',
+      theme: ThemeData(),
       home: Scaffold(
           backgroundColor: const Color.fromARGB(255, 248, 248, 248),
           appBar: AppBar(
-            title: Text('SolarSmart'),
-            backgroundColor: const Color.fromARGB(255, 200, 221, 46),
+            leading: Icon(Icons.menu),
+            title: Text('Solar Smart'),
+            actions: [
+              Icon(Icons.notifications),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.language_outlined),
+              ),
+              Icon(Icons.more_vert),
+            ],
+            backgroundColor: const Color.fromARGB(255, 248, 248, 248),
           ),
           body: SafeArea(
             child: Container(
               height: 100,
               width: 500,
-              margin: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
+              margin: EdgeInsets.symmetric(horizontal: 10),
               padding: EdgeInsets.all(20),
               color: Colors.amber,
               child: Text('We Are Risskovtekik'),
